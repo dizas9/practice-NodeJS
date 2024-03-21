a9da427eccbc805a01641ebe0d820ba8cc450cdf
Md. Sazidul Islam <sazid.ice07@gmail.com>
2024-03-21 05:39:18 +0000

    Session Intro with 'connect-mongodb-session' && 'express-session'

--- server/index.js
+++ server/index.js
@@ -1,8 +1,44 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+
+const mongoURI =
+  "mongodb+srv://dizas9:yoyo@cluster0.kxj2bsu.mongodb.net/?retryWrites=true&w=majority";
+
+//connect mongodb
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//session Store
+const store = new MongoDBStore({
+  uri: mongoURI,
+  databaseName: "Session",
+  collection: "session",
+});
+
+app.use(
+  session({
+    secret: "hello secret",
+    cookie: {
+      maxAge: 5000,
+      httpOnly: false,
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
 
 app.get("/", (req, res) => {
-  res.send("Hello Express again");
+  res.send("Hello Express again" + JSON.stringify(req.session));
 });
 
 app.listen(3000, () => {
